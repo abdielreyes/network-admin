df52be46e45bd6a615991ffa7b44cc8d83ba676b
KyZerark <neftvc@hotmail.com>
2022-04-03 20:16:16 -0500

    Finalizando con los usuarios

--- rutas/users.py
+++ rutas/users.py
@@ -17,7 +17,24 @@ def user_add():
 		print(resultado)
 	return "Usuario registrado exitosamente"
 
-#def user_edit():
+def user_edit():
+	if request.method == 'GET':
+		return render_template("editUser.html")
+	if request.method =='POST':
+		usr = request.form['nameUser']
+		con = request.form['passUser']
+
+		newUsr = request.form['newNameUser']
+		newCon = request.form['newPassUser']
+
+		comandos = ["no username "+usr+" privilege 15 password "+con]
+		resultado = connect_ssh("10.0.1.254","cisco","cisco",comandos)
+		print(resultado)
+
+		comandos2 = ["username "+newUsr+" privilege 15 password "+newCon]
+		resultado2 = connect_ssh("10.0.1.254","cisco","cisco",comandos2)
+		print(resultado2)
+	return "Cambios exitosos"
 
 def user_remove():
 	if request.method == 'GET':
